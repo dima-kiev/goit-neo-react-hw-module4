95663a01408171d3be49fb60d166f6e48b68c6e0
dima-kiev <dima.kiev@gmail.com>
2025-08-16 21:31:45 +0300

    hw04 upd api key

--- src/images-api.js
+++ src/images-api.js
@@ -4,7 +4,7 @@ axios.defaults.baseURL = "https://api.unsplash.com";
 
 export const fetchImagesAsync = async (searchText, currentPage) => {
   const response = await axios.get(
-    `/search/photos?query=${searchText}&page=${currentPage}&per_page=12&orientation=landscape&client_id=2KbPjCexKAkvZ-hfRES_cQq0WejpaRfqyViAWj-K8z4
+    `/search/photos?query=${searchText}&page=${currentPage}&per_page=12&orientation=landscape&client_id=2KbPjCexKAkvZ-hfRES_cQq0WejpaRfqyViAWj-K8z4`
   );
 
   return response.data;
